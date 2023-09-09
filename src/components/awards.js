61c719ff230a5de27437110fba76a479c1a39e46
Sameer <thisissameer6@gmail.com>
2023-09-09 16:15:52 +0530

    a

--- src/components/awards.js
+++ src/components/awards.js
@@ -4,7 +4,6 @@ function Awards() {
   const data = [
     { id: 1, description: "The Finest Fintech App Development Companies 2020 ", image: "https://terralogic.com/wp-content/uploads/2023/08/Inc5000.svg", byText: "By Inc" },
     { id: 2, description: "CMMI® Level3 maturity for India & Vietnam ", image: "https://terralogic.com/wp-content/uploads/2023/08/Inc5000.svg", byText: "By Inc" },
-    { id: 3, description: "DNA Paris Design Winner Award of 2022", image: "https://terralogic.com/wp-content/uploads/2022/09/DNA-award-1.png", byText: "BY DNA PARIS DESIGN" },
     { id: 4, description: "Redefining Trends of IoT Through Digital Transformation ", image: "https://terralogic.com/wp-content/uploads/2022/09/ceo-1.png", byText: "BY CEO Magazine" },
     { id: 5, description: "The Finest Fintech App Development Companies 2020", image: "https://terralogic.com/wp-content/uploads/2022/09/top-developers.png", byText: "BY TOP DEVELOPERS" },
     { id: 6, description: "CMMI® Level3 maturity for India & Vietnam", image: "https://terralogic.com/wp-content/uploads/2022/09/cmmi.png", byText: "By CMMI Level3" },
@@ -26,7 +25,7 @@ function Awards() {
       <div className='cards'>
         {data.map(s => (
           <div key={s.id} >
-            <img src={s.image} alt="s"/>
+            <div className='award-img'><img src={s.image} alt="s"/></div>
             <p>{s.description}</p>
             <span>⎯⎯ {s.byText}</span>
           </div>
